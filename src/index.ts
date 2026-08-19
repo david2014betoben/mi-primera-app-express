28aaf74f6af56dd8086ed8fa398c7a16098dae09
david2014betoben <davidgerardonunezrojas@gmail.com>
2026-08-18 20:23:16 -0400

    feat: CRUD de estudiantes en memoria

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,17 @@ import path from "node:path";
 const app = express();
 const PORT = 3000;
 
-async function obtenerDatos(): Promise<string> {
-  const ruta = path.resolve("src/status.json");
-  const texto = await fs.readFile(ruta, "utf-8");
-  return JSON.parse(texto);
+//MIDDLEWARE
+app.use(express.json());
+
+interface Estudiante {
+  id: number;
+  nombre: string;
+  email: string;
+  bootcamp: string;
 }
+
+let estudiantes: Estudiante[] = [];
 
 app.get("/", function (req: Request, res: Response) {
   res.send("El servidor esta en pie");
@@ -21,6 +27,91 @@ app.get("/api/status", async (req: Request, res: Response) => {
   const status = JSON.parse(datos);
 
   res.json(status);
+});
+
+app.get("/estudiantes", async function (req: Request, res: Response) {
+  res.json(estudiantes);
+});
+
+//endpoint id especifico
+app.get("/estudiantes/:id", async function (req: Request, res: Response) {
+  let idBuscado = Number(req.params.id);
+  const encontrado = estudiantes.filter((e) => e.id === idBuscado);
+  if (encontrado.length > 0) {
+    res.json(encontrado);
+  } else {
+    return res.status(404).json({ error: "Estudiante no encontrado" });
+  }
+});
+
+//CREAR UN ESTUDIANTE NUEVO METODO POST
+interface crearEstudiante {
+  nombre: string;
+  email: string;
+  bootcamp: string;
+}
+
+app.post(
+  "/estudiantes",
+  function (req: Request<{}, {}, crearEstudiante>, res: Response) {
+    const { nombre, email, bootcamp } = req.body;
+    if (!nombre || !email || !bootcamp) {
+      return res
+        .status(400)
+        .json({ error: "faltan datos que son obligatorios" });
+    }
+    const nuevoEstudiante: Estudiante = {
+      id: estudiantes.length > 0 ? estudiantes.length + 1 : 1,
+      nombre,
+      email,
+      bootcamp,
+    };
+    estudiantes.push(nuevoEstudiante);
+    res.status(201).json(nuevoEstudiante);
+  },
+);
+
+//Actualizar estudiante
+interface actualizarEstudiante {
+  nombre: string;
+  email: string;
+  bootcamp: string;
+}
+
+app.put("/estudiantes/:id", function (req: Request, res: Response) {
+  const idBuscado = Number(req.params.id);
+  const index = estudiantes.findIndex(function (e) {
+    return e.id === idBuscado;
+  });
+  if (index === -1) {
+    return res.status(404).json({ error: "Estudiante no encontrado" });
+  } else {
+    const { nombre, email, bootcamp }: actualizarEstudiante = req.body;
+
+    estudiantes[index] = {
+      id: idBuscado,
+      nombre: nombre ?? estudiantes[index]?.nombre,
+      email: email ?? estudiantes[index]?.email,
+      bootcamp: bootcamp ?? estudiantes[index]?.bootcamp,
+    };
+    res.json(estudiantes[index]);
+  }
+});
+
+//eliminar estudiante
+app.delete("/estudiantes/:id", function (req: Request, res: Response) {
+  const idBuscado = Number(req.params.id);
+  const index = estudiantes.findIndex(function (e) {
+    return e.id === idBuscado;
+  });
+  if (index === -1) {
+    return res
+      .status(404)
+      .json({ error: "estudiante no encontrado no podemos eliminarlo" });
+  } else {
+    estudiantes = estudiantes.filter((e) => e.id !== idBuscado);
+    res.json({ mensaje: "ESTUDIANTE ELIMINADO EXITOSAMENTE" });
+  }
 });
 
 app.listen(PORT, () => {
